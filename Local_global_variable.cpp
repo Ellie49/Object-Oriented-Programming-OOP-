cb28e7d8f3a617c1ced37413203d207e53d4a789
Farhana Ahmmed <farhanaellie49@gmail.com>
2024-09-05 22:12:11 +0600

    Create Local_global_variable.cpp

---
+++ Local_global_variable.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll  = long long;
+
+ll glo=6;
+void sum(){
+    ll glo=9;
+    cout << glo << endl;
+}
+int main(){
+   ll glo = 7;
+    glo = 78;
+    cout << glo << endl;
+    sum();
+    cout << glo << endl;
+}
